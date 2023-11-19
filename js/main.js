2bfc7631bf4d4ee8f6eb76fa86150cf401ef0a14
Hodayousry2752 <75079203+Hodayousry2752@users.noreply.github.com>
2023-11-19 03:39:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+
+let outerOne=document.getElementById("outer-one");
+let outerTwo=document.getElementById("outer-two");
+let outerThree=document.getElementById("outer-three");
+let reelResult=document.getElementById('reel-result');
+let htmlBtn=document.getElementById('html-btn');
+let cssBtn=document.getElementById('css-btn');
+let jsBtn=document.getElementById('js-btn');
+let resultBtn=document.getElementById('result-btn');
+let codeHtml=document.getElementById('code-html');
+let codeCss=document.getElementById('code-css');
+let codeJs=document.getElementById('code-js');
+
+
+
+let btns =document.getElementById('btns');
+let body =document.getElementById('body');
+
+
+function show(id){
+
+    outerOne.style.display='none';
+    outerThree.style.display='none';
+    outerTwo.style.display='none';
+    reelResult.style.display='none';
+    document.getElementById(id).style.display="block";
+
+}
+
+function show_all(){
+    if(window.innerWidth>=992)
+    {
+        outerOne.style.display="block";
+        outerTwo.style.display="block";
+        outerThree.style.display="block";
+        reelResult.style.display='block';
+    }else if(window.innerWidth<992 && outerOne.style.display=="block")
+    {
+        outerTwo.style.display="none";
+        outerThree.style.display="none";
+        reelResult.style.display='none';
+
+    }
+}
+
+function textareaValues(){
+    codeHtml.value=`<div>
+
+</div>`;
+codeCss.value=`<style>
+
+</style>`;
+codeJs.value=`<script>
+
+</script>`;
+}textareaValues();
+
+
+
+function resultOfCode() {
+  
+    let iframe = document.createElement('iframe');
+    
+    iframe.style.width = "100%";
+    iframe.style.height = "100vh";
+    iframe.style.backgroundColor = "white";
+  
+
+    reelResult.innerHTML = '';
+    reelResult.appendChild(iframe);
+
+    const iframeDoc = iframe.contentDocument;
+
+    iframeDoc.open();
+    iframeDoc.write(codeHtml.value+codeCss.value+codeJs.value);
+    iframeDoc.close();
+  }
